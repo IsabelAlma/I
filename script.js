bbd801ae5ef6fee166a9b81b402f6cfeadf9d2ec
IsabelAlma <IsabelAlma@users.noreply.github.com>
2020-06-16 20:56:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "150px";
+}
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
